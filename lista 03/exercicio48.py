16eff666eaa5b045f24dc9cf4a0b918ca941d8ba
Davi Aulino Tonin <126027403+daviaulinotonin@users.noreply.github.com>
2026-09-21 22:47:18 +0000

    deus é bom o tempo todo

--- lista 03/exercicio48.py
+++ lista 03/exercicio48.py
@@ -1,7 +1,7 @@
 ingressos = 5
 comprados = 0
 sistema = int(input("Deseja iniciar a compra?\n1 - Sim \n2 - Não\n> "))
-if sistema >2:
+while sistema >2:
     print('Opção inválida.')
     print('-'*15)
     sistema = int(input('Deseja tentar novamente?\n1 - Sim \n2 - Não\n'))
@@ -13,14 +13,15 @@ while sistema == 1:
         print('Não é possível comprar mais ingressos que o número disponível.')
         print('-'*15)
         sistema = int(input('Deseja tentar novamente?\n1 - Sim \n2 - Não\n'))
+        continue
     print(f'Você comprou {qnt} ingresso(s).')
     print('-'*15)
     ingressos -= qnt
     print(f'Ingressos disponíveis: {ingressos}')
-    sistema = int(input('Deseja comprar mais ingressos?\n1 - Sim \n2 - Não\n'))
     if ingressos == 0:
         print('Os ingressos para está obra acabaram.')
         break
+    sistema = int(input('Deseja comprar mais ingressos?\n1 - Sim \n2 - Não\n'))
 print('Sistema finalizado.')
 
 
